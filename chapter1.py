fc2ab3ba8c61bbf17f13fa74921a199944f3e344
julian <juliandavid.alfonso@utp.edu.co>
2021-09-12 22:45:54 -0500

    convert image to gray

--- chapter1.py
+++ chapter1.py
@@ -1,12 +1,9 @@
 
 import cv2
 
-cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
+img = cv2.imread('./Resources/lena.png')
 
-while True:
-    success, img = cap.read()
-    cv2.imshow('Video', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Gray image', imgGray)
+cv2.waitKey(0)
