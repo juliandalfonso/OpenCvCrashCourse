8fe190180e560a43a77c40471caf44655b4c9f6f
julian <juliandavid.alfonso@utp.edu.co>
2021-09-12 22:48:13 -0500

    desenfoca imagen

--- chapter1.py
+++ chapter1.py
@@ -3,7 +3,12 @@ import cv2
 
 img = cv2.imread('./Resources/lena.png')
 
+#convierte a escala de grises
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+#convierte a desenfocado
+imgBlur = cv2.GaussianBlur(imgGray, (7,7),0)
+
 cv2.imshow('Gray image', imgGray)
+cv2.imshow('image Blur', imgBlur)
 cv2.waitKey(0)
