c0c080ea7409d976a3ddf29274abf47deee9a3f6
julian <juliandavid.alfonso@utp.edu.co>
2021-09-12 22:54:17 -0500

    muestra los bordes de una imagen

--- chapter1.py
+++ chapter1.py
@@ -9,6 +9,11 @@ imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #convierte a desenfocado
 imgBlur = cv2.GaussianBlur(imgGray, (7,7),0)
 
+#muestra las bordes de la imagen
+imgCanny = cv2.Canny(img, 100,100)
+
+
 cv2.imshow('Gray image', imgGray)
 cv2.imshow('image Blur', imgBlur)
+cv2.imshow('image Canny', imgCanny)
 cv2.waitKey(0)
