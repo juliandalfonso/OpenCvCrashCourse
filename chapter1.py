1a8031bebc877f4a2c988c7712e4df253b49cff7
julian <juliandavid.alfonso@utp.edu.co>
2021-09-13 22:58:45 -0500

    recortamos imagen con vector

--- chapter1.py
+++ chapter1.py
@@ -7,9 +7,17 @@ img = cv2.imread('./Resources/lambo.png')
 #imprime el tamaño de la imagen
 #(alto, ancho, #de canales(3=BGR))
 print(img.shape)
-imgResize = cv2.resize(img,(300, 200))
+
+#redimensionamos al tamaño que queremos (alto, ancho)
+imgResize = cv2.resize(img,(1000, 500))
+
+#recortamos una imagen desde 0y hasta 200y y desde 200x hasta 500x
+imgCropped = img[0:200,200:500]
 
 cv2.imshow('image', img)
 cv2.imshow('image resized', imgResize)
+cv2.imshow('image cropped', imgCropped)
+
+
 
 cv2.waitKey(0)
