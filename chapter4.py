60c10400fb8ad3f9f2c4172ffdc3841abf0858b5
julian <juliandavid.alfonso@utp.edu.co>
2021-09-14 19:27:29 -0500

    agrega linea verde al display

--- chapter4.py
+++ chapter4.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 
 img = np.zeros((512,512,3),np.uint8)
-print(img)
+cv2.line(img,(20,20),(300,300),(0,255,0),3)
 
 #[:] coge todos los pixeles de la imagen
-img[30:200,30:200]= 255,0,0
+#img[:]= 255,0,0
 
 cv2.imshow('image', img)
 
