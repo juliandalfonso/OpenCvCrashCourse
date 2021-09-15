8d4aea7a1fbd2a7ab7d78a1b02552167e5cb7858
julian <juliandavid.alfonso@utp.edu.co>
2021-09-14 19:29:05 -0500

    linea diagonal por con alto y ancho de la imagen

--- chapter4.py
+++ chapter4.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 img = np.zeros((512,512,3),np.uint8)
-cv2.line(img,(20,20),(300,300),(0,255,0),3)
+cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
 
 #[:] coge todos los pixeles de la imagen
 #img[:]= 255,0,0
