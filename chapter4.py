e562232040975f0c4d2a37a7f26d599e75537862
julian <juliandavid.alfonso@utp.edu.co>
2021-09-14 19:30:43 -0500

    agrega un cuadrado a la imagen

--- chapter4.py
+++ chapter4.py
@@ -3,6 +3,7 @@ import numpy as np
 
 img = np.zeros((512,512,3),np.uint8)
 cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
+cv2.rectangle(img,(0,0),(250,350),(0,0,255),2)
 
 #[:] coge todos los pixeles de la imagen
 #img[:]= 255,0,0
