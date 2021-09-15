a6a91582fa587b52c8392a7395b0e9bd18f2ea36
julian <juliandavid.alfonso@utp.edu.co>
2021-09-14 21:47:59 -0500

    agrega circulo a la pantalla

--- chapter4.py
+++ chapter4.py
@@ -5,6 +5,8 @@ img = np.zeros((512,512,3),np.uint8)
 cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
 cv2.rectangle(img,(0,0),(250,350),(0,0,255),2)
 
+cv2.circle(img,(400,50),30,(255,255,0),5)
+
 #[:] coge todos los pixeles de la imagen
 #img[:]= 255,0,0
 
