c0443e23953ad3b683643d51c8f5eded88435a25
julian <juliandavid.alfonso@utp.edu.co>
2021-11-01 20:48:27 -0500

    controladores de H,S y V agregados

--- chapter7.py
+++ chapter7.py
@@ -1,6 +1,26 @@
 import cv2
+
+
+
+def empty(a):
+    pass
+
+
+
 path = 'Resources/lambo.png'
+cv2.namedWindow('Trackbars')
+cv2.resizeWindow("Trackbars", 640, 240)
+cv2.createTrackbar("Hue min", "Trackbars",0,179,empty)
+cv2.createTrackbar("Hue max", "Trackbars",179,179,empty)
+cv2.createTrackbar("sat min", "Trackbars",0,255,empty)
+cv2.createTrackbar("sat max", "Trackbars",255,255,empty)
+cv2.createTrackbar("val min", "Trackbars",0,255,empty)
+cv2.createTrackbar("val max", "Trackbars",255,255,empty)
+
+
+
 img =cv2.imread(path)
+
 
 imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
